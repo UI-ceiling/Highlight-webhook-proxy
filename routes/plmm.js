ad6db06aabe20d5be18c5c08613f2aa1c7704380
任梓豪 <ro_ren@gomro.cn>
2025-04-30 14:00:07 +0800

    feat(plmm): 添加 push_plmm 函数的错误捕获和企业微信告警
    - 在 router.get 中添加 try-catch 块，捕获 push_plmm 函数的错误
    - 使用 fetch 发送错误信息到企业微信 webhook
    - 指定告警发送给特定手机号的用户

--- routes/plmm.js
+++ routes/plmm.js
@@ -4,7 +4,25 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-  await push_plmm();
+  try{
+    await push_plmm();
+  }catch(error){
+    const webhookURL = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=d074b6c5-c660-4c30-88ee-114dc76f3a98'; // Replace this
+
+    const response = await fetch(webhookURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "msgtype": "text",
+        "text": {
+          "content": `push error: ${error.message}`,
+          "mentioned_mobile_list":["18233279029"]
+        }
+      }),
+    });
+  }
   res.send('推送plmm');
 });
 
